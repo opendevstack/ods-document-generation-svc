1958941c331d45cbd89d7b64b7f7381be5e532ee
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-05-04 10:29:00 +0200

    add native github test as well

--- src/test/groovy/app/GithubDocumentTemplatesStoreSpec.groovy
+++ src/test/groovy/app/GithubDocumentTemplatesStoreSpec.groovy
@@ -9,7 +9,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*
 
 class GithubDocumentTemplatesStoreSpec extends SpecHelper {
 
-    def "getTemplatesForVersion"() {
+    def "getTemplatesForVersionMock"() {
         given:
         
         env.set("GITHUB_HOST", "http://localhost:9001")
@@ -28,6 +28,27 @@ class GithubDocumentTemplatesStoreSpec extends SpecHelper {
         Paths.get(path.toString(), "templates", "header.inc.html.tmpl").toFile().exists()
         Paths.get(path.toString(), "templates", "TIP.html.tmpl").toFile().exists()
 
+        cleanup:
+         targetDir.toFile().deleteDir()
+         env.clear("GITHUB_HOST")
+    }
+
+    def "getTemplatesForVersion"() {
+        given:
+        
+        def store = new GithubDocumentTemplatesStore()
+        def targetDir = Files.createTempDirectory("doc-gen-templates-")
+        def version = "1.0"
+  
+        when:
+        def path = store.getTemplatesForVersion(version, targetDir)
+        
+        then:
+        Paths.get(path.toString(), "templates").toFile().exists()
+        Paths.get(path.toString(), "templates", "footer.inc.html.tmpl").toFile().exists()
+        Paths.get(path.toString(), "templates", "header.inc.html.tmpl").toFile().exists()
+        Paths.get(path.toString(), "templates", "TIP.html.tmpl").toFile().exists()
+  
         cleanup:
          targetDir.toFile().deleteDir()
     }
