5940ed20fdd4f590137f639a4fd6cc5545563409
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2021-11-18 08:40:07 +0100

    Use Markdown Architectural Decision Records https://adr.github.io/madr/ Improve maintainability by adding SpringBoot framework Added IT (Docker tests) Added performance tests (Gatling) logback.xml can be overridden from command line

---
+++ src/main/groovy/org/ods/doc/gen/templates/repository/DocumentTemplateFactory.groovy
@@ -0,0 +1,20 @@
+package org.ods.doc.gen.templates.repository
+
+import org.springframework.stereotype.Repository
+
+import javax.inject.Inject
+
+@Repository
+class DocumentTemplateFactory {
+
+    private final List<DocumentTemplatesRepository> docTemplates
+
+    @Inject
+    DocumentTemplateFactory(List<DocumentTemplatesRepository> docTempls){
+        this.docTemplates = docTempls
+    }
+
+    DocumentTemplatesRepository get(){
+        return docTemplates.get(0).isApplicableToSystemConfig() ? docTemplates.get(0) : docTemplates.get(1)
+    }
+}
---
+++ src/test/groovy/org/ods/doc/gen/AppConfig.groovy
@@ -0,0 +1,13 @@
+package org.ods.doc.gen
+
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.PropertySource
+
+@TestConfiguration
+@PropertySource("classpath:application.properties")
+@ComponentScan("org.ods.doc.gen")
+class AppConfig {
+
+}
