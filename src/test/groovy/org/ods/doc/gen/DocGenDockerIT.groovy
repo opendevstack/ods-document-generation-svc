c7ff4b63d3d899d0f5f618b73c74c37c3d96bab5
Sergio Sacristán <extra@sacristan.com.es>
2022-03-16 09:19:01 +0100

    Feature/move leva doc gradle (#80)
    * improve  gradle understanding

--- src/test/groovy/org/ods/doc/gen/DocGenDockerIT.groovy
+++ src/test/groovy/org/ods/doc/gen/DocGenDockerIT.groovy
@@ -20,11 +20,17 @@ import static org.hamcrest.Matchers.equalTo
 class DocGenDockerIT extends Specification {
 
     static final DockerImageName DOCGEN_IMAGE = DockerImageName.parse("ods-document-generation-svc:local");
+    static final int SERVER_PORT = 1111
+    static final String SERVER_PORT_CHANGED_WITH_ENV_VAR = "8080"
+    static final String ROOT__LOG__LEVEL = "ROOT_LOG_LEVEL"
+    static final String TRACE = "TRACE"
+    static final String SERVER_PORT_NAME = "SERVER_PORT"
 
     @Shared
     GenericContainer<?> docGenContainer = new GenericContainer<>(DOCGEN_IMAGE)
-            .withExposedPorts(1111)
-            .withEnv("ROOT_LOG_LEVEL", "TRACE")
+            .withExposedPorts(SERVER_PORT)
+            .withEnv(ROOT__LOG__LEVEL, TRACE)
+            .withEnv(SERVER_PORT_NAME, SERVER_PORT_CHANGED_WITH_ENV_VAR)
 
     def "docgen is running in docker"() {
         given:
